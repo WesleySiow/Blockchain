fa2b17a3b8ab5deb7a2531263c649981234dcfdc
Wesley Siow <wesleysiow@gmail.com>
2019-05-09 10:19:06 -0400

    Create Faucet.sol

---
+++ Faucet.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+//Our first contract is a faucet
+contract Faucet {
+    
+    //Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+    
+        //Limit withdrawal amount    
+        require(withdraw_amount <=100000000000000000);
+        
+        //Sned the amount to the address that requested it 
+        msg.sender.transfer(withdraw_amount);
+    }
+    
+    //Accept any incoming amount
+     function () external payable {}
+}
